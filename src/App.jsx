cc7cc655274927db534a8f8ae90c24ab93e6ac4e
Patopam <145809595+Patopam@users.noreply.github.com>
2025-04-07 15:55:17 -0500

    Feat

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './screens/dashboard/dashboard';
 import Emotions from './screens/emotions/emotions';
+import Finance from './screens/finance/finance';
 // Importa otras pantallas si las tienes
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
 			<Routes>
 				<Route path='/' element={<Dashboard />} />
 				<Route path='/emotions' element={<Emotions />} />
+				<Route path='/finance' element={<Finance />} />
 			</Routes>
 		</Router>
 	);
