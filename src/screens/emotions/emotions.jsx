c3aa26bb44e61217bbfad11c856137279f613b1a
Patopam <145809595+Patopam@users.noreply.github.com>
2025-04-06 20:01:29 -0500

    Feat

--- src/screens/emotions/emotions.jsx
+++ src/screens/emotions/emotions.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import MoodTracker from '../../components/Tables/mood';
 import Menu from '../../components/Menu/menu';
+import ReminderCard from '../../components/Cards/remainder';
+import GoalProgressCard from '../../components/Cards/goal';
 
 function Emotions() {
 	return (
 		<div className='emotions-container'>
 			<Menu />
 			<MoodTracker />
+			<ReminderCard />
+			<GoalProgressCard />
 		</div>
 	);
 }
