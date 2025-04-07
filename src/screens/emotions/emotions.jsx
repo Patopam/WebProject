1465474e6d43fa48f58314bdc5d486bf2f6c0742
Patopam <145809595+Patopam@users.noreply.github.com>
2025-04-06 20:06:33 -0500

    Feat

--- src/screens/emotions/emotions.jsx
+++ src/screens/emotions/emotions.jsx
@@ -3,6 +3,10 @@ import MoodTracker from '../../components/Tables/mood';
 import Menu from '../../components/Menu/menu';
 import ReminderCard from '../../components/Cards/remainder';
 import GoalProgressCard from '../../components/Cards/goal';
+import Header from '../../components/Header/header';
+import LogoutIcon from '@mui/icons-material/Logout';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import CustomIconButton from '../../components/Buttons/icon';
 
 function Emotions() {
 	return (
@@ -11,6 +15,13 @@ function Emotions() {
 			<MoodTracker />
 			<ReminderCard />
 			<GoalProgressCard />
+			<div className='header-section'>
+				<Header title='Welcome Evan!' subtitle='How are you feeling today?' emoji='😊' />
+				<div className='icon-buttons'>
+					<CustomIconButton icon={<AccountCircleIcon />} ariaLabel='user' />
+					<CustomIconButton icon={<LogoutIcon />} ariaLabel='logut' />
+				</div>
+			</div>
 		</div>
 	);
 }
