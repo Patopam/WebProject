b870111de49f7c104d86b0bd20bbd90d96d54035
alejahurtd <mairahurtado930@gmail.com>
2025-04-06 20:21:29 -0500

    feat: Add FeelingsCard component

--- src/screens/dashboard/dashboard.jsx
+++ src/screens/dashboard/dashboard.jsx
@@ -1,4 +1,3 @@
-// src/screens/Dashboard.jsx
 import React from 'react';
 import AddButton from '../../components/Buttons/add';
 import Header from '../../components/Header/header';
@@ -8,23 +7,23 @@ import EmotionWeek from '../../components/Cards/emotionWeek';
 import CustomIconButton from '../../components/Buttons/icon';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import LogoutIcon from '@mui/icons-material/Logout';
+import FeelingsCard from '../../components/Cards/FeelingsCard';
 import Menu from '../../components/Menu/menu';
 
 function Dashboard() {
 	// Funciones para manejar los clics
 	const handleJournalClick = () => {
 		console.log('Daily journal clicked');
-		// Tu lógica aquí
 	};
 
 	const handleSpendClick = () => {
 		console.log('Add spend clicked');
-		// Tu lógica aquí
 	};
 
 	return (
 		<div className='dashboard-container'>
 			<Menu />
+			<FeelingsCard />
 			<Header title='Welcome Evan!' subtitle='How are you feeling today?' emoji='😊' />
 			<CustomIconButton icon={<AccountCircleIcon />} ariaLabel='user' />
 			<CustomIconButton icon={<LogoutIcon />} ariaLabel='logut' />
