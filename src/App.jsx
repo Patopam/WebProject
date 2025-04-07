8bc40cce44f541abb088921e0c44dfdf92cc0461
Patopam <145809595+Patopam@users.noreply.github.com>
2025-04-06 21:36:12 -0500

    Style

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,6 @@ function App() {
 			<Routes>
 				<Route path='/' element={<Dashboard />} />
 				<Route path='/emotions' element={<Emotions />} />
-				{/* Añade las demás rutas según necesites */}
 			</Routes>
 		</Router>
 	);
