b5149d47747c1e9b115e9b7a038ef379970c5267
Patopam <145809595+Patopam@users.noreply.github.com>
2025-04-06 22:47:07 -0500

    Update dashboard.jsx

--- src/screens/dashboard/dashboard.jsx
+++ src/screens/dashboard/dashboard.jsx
@@ -43,7 +43,7 @@ function Dashboard() {
 				<div className='dashboard-cards-row'>
 					<ReminderCard title='Know thyself, transform your life.' />
 					<FeelingsCard />
-					<GoalProgressCard spent={150000} total={200000} compact={true} />
+					<GoalProgressCard spent={150000} total={200000} />
 				</div>
 
 				{/* Bottom row with expenses table on left and emotion week on right */}
