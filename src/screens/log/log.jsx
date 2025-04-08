842d901ce200fb8c2748e4bb276f04729b2cc760
klimga2 <poker8702@gmail.com>
2025-04-08 14:34:48 -0500

    Agrege Forms

--- src/screens/log/log.jsx
+++ src/screens/log/log.jsx
@@ -3,15 +3,48 @@ import BotonStart from "../../components/Buttons/botonesStart";
 import BotonStartGoogle from "../../components/Buttons/botonStartGoogle";
 import { useNavigate } from "react-router";
 
+import { Password } from "primereact/password";
+
+import { InputText } from "primereact/inputtext";
+
 function Log() {
   return (
     <>
       <Container sx={{ width: 500 }}>
+        <Box>
+          <form>
+            <InputText
+              value={value}
+              onChange={(e) => setValue(e.target.value)}
+            />
+            <Password
+              value={value}
+              onChange={(e) => setValue(e.target.value)}
+              feedback={false}
+              tabIndex={1}
+            />
+          </form>
+        </Box>
         <Box sx={{ width: 460 }}>
           <Stack spacing={3}>
             <BotonStart text="Log In" />
             <BotonStartGoogle text="Connect with Google" />
           </Stack>
+          <Typography
+            sx={{
+              color: "var(--Neutral-1000, #333)",
+              fontFamily: "Manrope, sans-serif",
+              fontSize: "20px",
+              fontStyle: "normal",
+              fontWeight: 400,
+              lineHeight: "normal",
+              color: "#000000",
+              textAlign: "center",
+              marginTop: "20px",
+            }}
+          >
+            No account? Create an account
+          </Typography>
         </Box>
       </Container>
     </>
