fd20fb0687aea3b57624a2afaa8efe77a2da2bd6
Patopam <145809595+Patopam@users.noreply.github.com>
2025-04-08 23:41:07 -0500

    Update journalData.js

--- src/Data/journalData.js
+++ src/Data/journalData.js
@@ -1,4 +1,3 @@
-// journalData.js
 export const journalData = [
 	{
 		week: 1,
@@ -8,14 +7,14 @@ export const journalData = [
 				month: 'Jan',
 				content:
 					"Today felt like a blur—so much to do, yet it feels like I barely moved forward. Maybe progress isn't always obvious in the moment. I just have to trust that each small step is adding up to something bigger.",
-				color: '#BFD18E', // verde claro
+				color: '#BFD18E',
 			},
 			{
 				day: 'Tuesday',
 				month: 'Jan',
 				content:
 					"Some days feel heavier than others, but even the smallest wins count. I'll be kind to myself and try again tomorrow.",
-				color: '#F8B195', // naranja claro
+				color: '#F8B195',
 			},
 		],
 	},
@@ -27,14 +26,14 @@ export const journalData = [
 				month: 'Feb',
 				content:
 					"Today felt like a blur—so much to do, yet it feels like I barely moved forward. Maybe progress isn't always obvious in the moment. I just have to trust that each small step is adding up to something bigger.",
-				color: '#A4C4F4', // azul claro
+				color: '#A4C4F4',
 			},
 			{
 				day: 'Tuesday',
 				month: 'Feb',
 				content:
 					"Some days feel heavier than others, but even the smallest wins count. I'll be kind to myself and try again tomorrow.",
-				color: '#D7C2E8', // púrpura claro
+				color: '#D7C2E8',
 			},
 		],
 	},
@@ -46,7 +45,7 @@ export const journalData = [
 				month: 'Mar',
 				content:
 					"Today felt like a blur—so much to do, yet it feels like I barely moved forward. Maybe progress isn't always obvious in the moment. I just have to trust that each small step is adding up to something bigger.",
-				color: '#F8E8A0', // amarillo claro
+				color: '#F8E8A0',
 			},
 		],
 	},
