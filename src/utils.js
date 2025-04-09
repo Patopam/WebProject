4941fb02a9ccb818d70590b6f18c2856d5d68adc
alejahurtd <mairahurtado930@gmail.com>
2025-04-09 13:41:36 -0500

    feat: implement  status filters in docs utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,18 +1,43 @@
 export const guardarUsuario = (usuario) => {
-  localStorage.setItem("usuario", JSON.stringify(usuario));
+	localStorage.setItem('usuario', JSON.stringify(usuario));
 };
 export const guardarCorreo = (correo) => {
-  localStorage.setItem("correo", JSON.stringify(correo));
+	localStorage.setItem('correo', JSON.stringify(correo));
 };
 
 export const guardarContraseña = (contraseña) => {
-  localStorage.setItem("contraseña", JSON.stringify(contraseña));
+	localStorage.setItem('contraseña', JSON.stringify(contraseña));
 };
 
 export const obtenerCorreo = () => {
-  return JSON.parse(localStorage.getItem("correo"));
+	return JSON.parse(localStorage.getItem('correo'));
 };
 
 export const obetenerContraseña = () => {
-  return JSON.parse(localStorage.getItem("contraseña"));
+	return JSON.parse(localStorage.getItem('contraseña'));
 };
+
+export function filterByTime(dateStr, selectedFilter) {
+	const today = new Date();
+	const itemDate = new Date(dateStr);
+
+	if (selectedFilter === 'Today') {
+		return (
+			itemDate.getDate() === today.getDate() &&
+			itemDate.getMonth() === today.getMonth() &&
+			itemDate.getFullYear() === today.getFullYear()
+		);
+	}
+
+	if (selectedFilter === 'Week') {
+		const diff = Math.abs(today - itemDate);
+		const daysDiff = diff / (1000 * 60 * 60 * 24);
+		return daysDiff <= 7;
+	}
+
+	if (selectedFilter === 'Month') {
+		return itemDate.getMonth() === today.getMonth() && itemDate.getFullYear() === today.getFullYear();
+	}
+
+	return true;
+}
