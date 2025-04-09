8d104d35028d8f92a713cc5fcb27571f2c5d8fa2
alejahurtd <mairahurtado930@gmail.com>
2025-04-09 17:23:47 -0500

    Feat: add use state for editing states

--- src/screens/finance/addSpending.jsx
+++ src/screens/finance/addSpending.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import EditInput from '../../components/Inputs/EditInput';
 import SendIcon from '@mui/icons-material/Send';
@@ -5,6 +6,16 @@ import CloseIcon from '@mui/icons-material/Close';
 
 const AddSpending = () => {
 	const navigate = useNavigate();
+
+	const [date, setDate] = useState('April 09 2025');
+	const [category, setCategory] = useState('Write here....');
+	const [price, setPrice] = useState('$20.000');
+	const [description, setDescription] = useState('Write here....');
+
+	const [editDate, setEditDate] = useState(false);
+	const [editCategory, setEditCategory] = useState(false);
+	const [editPrice, setEditPrice] = useState(false);
+	const [editDesc, setEditDesc] = useState(false);
 
 	const handleClose = () => navigate('/finance');
 
@@ -16,13 +27,37 @@ const AddSpending = () => {
 			</div>
 
 			<div>
-				<EditInput label='Add date' />
-				<EditInput label='Add Category' />
+				<EditInput
+					label='Add date'
+					value={date}
+					onChange={(e) => setDate(e.target.value)}
+					editable={editDate}
+					onEditClick={() => setEditDate(!editDate)}
+				/>
+				<EditInput
+					label='Add Category'
+					value={category}
+					onChange={(e) => setCategory(e.target.value)}
+					editable={editCategory}
+					onEditClick={() => setEditCategory(!editCategory)}
+				/>
 			</div>
 
-			<EditInput label='Add Price' />
+			<EditInput
+				label='Add Price'
+				value={price}
+				onChange={(e) => setPrice(e.target.value)}
+				editable={editPrice}
+				onEditClick={() => setEditPrice(!editPrice)}
+			/>
 
-			<EditInput label='Description' />
+			<EditInput
+				label='Description'
+				value={description}
+				onChange={(e) => setDescription(e.target.value)}
+				editable={editDesc}
+				onEditClick={() => setEditDesc(!editDesc)}
+			/>
 
 			<button>
 				<SendIcon />
