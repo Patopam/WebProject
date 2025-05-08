3e0da7a8b45e800e031c58c31158af646f87c10b
klimga2 <poker8702@gmail.com>
2025-05-07 20:40:23 -0500

    Update start.jsx

--- src/screens/start/start.jsx
+++ src/screens/start/start.jsx
@@ -1,50 +1,54 @@
-import { Typography, Container, Stack, Box } from '@mui/material';
-import BotonStart from '../../components/Buttons/botonesStart';
-import Logo from '../../assets/Sense Logo start.png';
-import BotonStartGoogle from '../../components/Buttons/botonStartGoogle';
-import { useNavigate } from 'react-router';
-import './start.css';
+import { Typography, Container, Stack, Box } from "@mui/material";
+import BotonStart from "../../components/Buttons/botonesStart";
+import Logo from "../../assets/Sense Logo start.png";
+import BotonStartGoogle from "../../components/Buttons/botonStartGoogle";
+import { useNavigate } from "react-router";
+import { useSelector } from "react-redux";
+import "./start.css";
 function Start() {
-	let navigate = useNavigate();
-	const goLogin = () => {
-		navigate('/log');
-	};
+  const auth = useSelector((state) => state.auth);
+  console.log(auth);
+  let navigate = useNavigate();
 
-	const goSign = () => {
-		navigate('/sing');
-	};
-	return (
-		<>
-			<Container sx={{ width: 500 }} className='container-start'>
-				<div className='menu-logo'>
-					<img src={Logo} alt='Sense Logo' className='logo-img' />
-				</div>
-				<Box sx={{ width: 460 }}>
-					<Stack spacing={3}>
-						<BotonStart text='Log In' onClick={goLogin} />
-						<BotonStart text='Create your account' onClick={goSign} />
-						<BotonStartGoogle text='Connect with Google' />
-					</Stack>
-				</Box>
-				<Box className='text-start'>
-					<Typography
-						sx={{
-							color: 'var(--Neutral-1000, #333)',
-							fontFamily: 'Manrope, sans-serif',
-							fontSize: '20px',
-							fontStyle: 'normal',
-							fontWeight: 400,
-							lineHeight: 'normal',
-							color: '#000000',
-							textAlign: 'center',
-						}}
-					>
-						A space that supports what you feel and how you choose.
-					</Typography>
-				</Box>
-			</Container>
-		</>
-	);
+  const goLogin = () => {
+    navigate("/log");
+  };
+
+  const goSign = () => {
+    navigate("/sing");
+  };
+  return (
+    <>
+      <Container sx={{ width: 500 }} className="container-start">
+        <div className="menu-logo">
+          <img src={Logo} alt="Sense Logo" className="logo-img" />
+        </div>
+        <Box sx={{ width: 460 }}>
+          <Stack spacing={3}>
+            <BotonStart text="Log In" onClick={goLogin} />
+            <BotonStart text="Create your account" onClick={goSign} />
+            <BotonStartGoogle text="Connect with Google" />
+          </Stack>
+        </Box>
+        <Box className="text-start">
+          <Typography
+            sx={{
+              color: "var(--Neutral-1000, #333)",
+              fontFamily: "Manrope, sans-serif",
+              fontSize: "20px",
+              fontStyle: "normal",
+              fontWeight: 400,
+              lineHeight: "normal",
+              color: "#000000",
+              textAlign: "center",
+            }}
+          >
+            A space that supports what you feel and how you choose.
+          </Typography>
+        </Box>
+      </Container>
+    </>
+  );
 }
 
 export default Start;
